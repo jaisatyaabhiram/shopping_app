7afbd6bcf4d06ff2d345e3ff581a8e86c53a97f3
jaisatyaabhiram <jaisatyaabhiram>
2026-01-04 10:27:32 +0530

    creating seperate product list widget

---
+++ lib/product_list_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/global_variables.dart';
+import 'package:shop_app_flutter/product_card.dart';
+import 'package:shop_app_flutter/product_details_page.dart';
+
+class ProductListPage extends StatefulWidget {
+  const ProductListPage({super.key});
+
+  @override
+  State<ProductListPage> createState() => _ProductListPageState();
+}
+
+class _ProductListPageState extends State<ProductListPage> {
+  final List<String> filter = const ["All", "Adidas", "Nike", "Puma"];
+  late String selectedFilter;
+  @override
+  void initState() {
+    // TODO: implement initState
+    selectedFilter = filter[0];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(30)),
+    );
+
+    return SafeArea(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Shoes \nCollection',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: border,
+                    enabledBorder: border,
+                    focusedBorder: border,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filter.length,
+              itemBuilder: (context, index) {
+                final label = Text(filter[index]);
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFilter = filter[index];
+                      });
+                    },
+                    child: Chip(
+                      label: label,
+                      labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                      backgroundColor: selectedFilter == filter[index]
+                          ? Theme.of(context).colorScheme.primary
+                          : Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      side: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return ProductDetailsPage(product: products[index]);
+                        },
+                      ),
+                    );
+                  },
+                  child: ProductCard(
+                    title: products[index]['title'].toString(),
+                    price: products[index]['price'] as double,
+                    image: products[index]['imageUrl'].toString(),
+                    backgroundcolor: index.isEven
+                        ? Color.fromRGBO(216, 240, 253, 1)
+                        : Color.fromRGBO(245, 247, 249, 1),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
